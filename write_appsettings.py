3b75efcd826d61aec90f391ccf9997df944fc61c
Frank Ndlovu <Frank.Ndlovu@fibrefi.co.za>
2026-05-21 10:50:37 +0200

    fix: move appsettings generation to python script file

---
+++ write_appsettings.py
@@ -0,0 +1,33 @@
+import os
+import json
+
+config = {
+    "ConnectionStrings": {
+        "DefaultConnection": os.environ["DB_CONNECTION_STRING"]
+    },
+    "Logging": {
+        "LogLevel": {
+            "Default": "Information",
+            "Microsoft.AspNetCore": "Warning"
+        }
+    },
+    "AllowedHosts": "*",
+    "Graph": {
+        "TenantId": os.environ["GRAPH_TENANT_ID"],
+        "ClientId": os.environ["GRAPH_CLIENT_ID"],
+        "ClientSecret": os.environ["GRAPH_CLIENT_SECRET"],
+        "SenderEmail": os.environ["SENDER_EMAIL"]
+    },
+    "SharePoint": {
+        "SiteUrl": os.environ["SHAREPOINT_SITE_URL"],
+        "ListId": os.environ["SHAREPOINT_LIST_ID"]
+    },
+    "App": {
+        "BaseUrl": os.environ.get("APP_BASE_URL", "http://10.200.0.76:8085")
+    }
+}
+
+with open("publish/appsettings.json", "w") as f:
+    json.dump(config, f, indent=2)
+
+print("appsettings.json written successfully")
